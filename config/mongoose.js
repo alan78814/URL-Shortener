83e1f68c458196b72a08709ee91280ed585d2015
alan78814 <tri78814@gmail.com>
2021-07-28 17:00:18 +0800

    fix: wrong word of mongoose.js to MONGODB_URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.mainUrl || 'mongodb://localhost/short-url-generator'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/short-url-generator'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
